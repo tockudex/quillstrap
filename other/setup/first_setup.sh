e6b48e0f4322a0b69a4b1bec1c60e151463dab5c
Szybet <53944559+Szybet@users.noreply.github.com>
2025-07-29 18:35:57 +0000

    rearrange things

---
+++ other/setup/first_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Run from the top of the quillstrap repo
+
+if ! command -v ~/.cargo/bin/cargo &> /dev/null || \
+   ! command -v ~/.cargo/bin/rustup &> /dev/null || \
+   ! command -v ~/.cargo/bin/cbindgen &> /dev/null; then
+       
+    echo "Installing Rust..."
+    curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
+    
+    source ~/.cargo/env
+    export PATH="$HOME/.cargo/bin:$PATH"
+
+    ~/.cargo/bin/cargo install --force cbindgen
+else
+    echo "Rust is already installed, skipping installation."
+fi
