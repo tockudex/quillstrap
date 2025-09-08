bb16232b59a9009275485b0099a3b4ae789f9d67
Nicolas Mailloux <nicolecrivain@gmail.com>
2025-09-08 14:19:06 +0000

    Small Mac change

--- other/setup/welcome_back.sh
+++ other/setup/welcome_back.sh
@@ -2,11 +2,11 @@
 if [ -e "/sys/module/rosetta" ]; then
     CARGO_DIR="root/.cargo"
     RUSTUP_DIR="root/.rustup"
-    if ! mountpoint "${CARGO_DIR}"; then
+    if ! mountpoint "/${CARGO_DIR}"; then
         mkdir -p "${CARGO_DIR}" "/${CARGO_DIR}"
         mount --bind "${CARGO_DIR}" "/${CARGO_DIR}"
     fi
-    if ! mountpoint "${RUSTUP_DIR}"; then
+    if ! mountpoint "/${RUSTUP_DIR}"; then
         mkdir -p "${RUSTUP_DIR}" "/${RUSTUP_DIR}"
         mount --bind "${RUSTUP_DIR}" "/${RUSTUP_DIR}"
     fi
